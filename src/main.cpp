901efb56e3f9553a20552643c13f1d8a48afcb86
Ayush1Sikarwar <ayushlegend1212@gmail.com>
2026-02-04 13:42:52 +0530

    update servo control logic in loop function

--- src/main.cpp
+++ src/main.cpp
@@ -4,9 +4,16 @@ Servo myServo;
 void setup() {
     Serial.begin(9600);
     myServo.attach(10); // Attach servo to pin 10
-    Serial.println("Servo ");
+    Serial.println("Servo initiate");
 }
 
 void loop() {
-// write your code here
+    myServo.write(0);
+    delay(500);
+    myServo.write(90);
+    delay(500);
+    myServo.write(120);
+    delay(500);
+    myServo.write(180);
+    delay(500);
 }
